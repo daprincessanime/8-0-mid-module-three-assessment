b9fa19ea230ade6e857b8ef4f0e17241331b0aea
Lakisha Trusty <daprincessanime@gmail.com>
2021-11-19 15:49:58 -0500

    finally started with the ClassName of the shopping, products and cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Component } from "react";
 import "./App.css";
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
+class App extends Component {
+  constructor(){
+    super ()
+  }
+
+render (){
+  return 
+  <div>
+      <h1>My Garage Sale</h1>;
+      <div className="product-container">
+        <div id="products">
+          <div>Name</div>
+          <div>description</div>
+          <img src="" alt=""/>
+        </div>
+
+      </div>
+      <div className="shopping-container">
+      <ul>
+            <li ></li>
+           
+            <li></li>
+          </ul>
+      </div>
+      <div className="checkout=container">
+        <label>
+          
+        </label>
+      </div>
+  </div>
 };
+}
 
 export default App;
